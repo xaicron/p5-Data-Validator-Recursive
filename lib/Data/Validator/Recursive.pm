ae948b5a693dcfcac8f8f9c2f52df78907f70007
xaicron <xaicron@gmail.com>
2013-02-23 18:08:21 +0900

    initial commit

---
+++ lib/Data/Validator/Recursive.pm
@@ -0,0 +1,42 @@
+package Data::Validator::Recursive;
+
+use strict;
+use warnings;
+use 5.008_001;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=for stopwords
+
+=head1 NAME
+
+Data::Validator::Recursive - blah blah blah
+
+=head1 SYNOPSIS
+
+  use Data::Validator::Recursive;
+
+=head1 DESCRIPTION
+
+Data::Validator::Recursive is
+
+=head1 AUTHOR
+
+Yuji Shimada E<lt>xaicron {@} GMAIL.COME<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2013 - Yuji Shimada
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
